254619922f98ce193270f60e5c5433af2550466d
Jakob Rasmus Henriksen <jarh@itu.dk>
2022-01-17 18:15:30 +0100

    Changed inline styling from css to tailwind css; Created a NavBar component where we can navigate to: Overview-page, trade-page, and the shop

---
+++ frontend/src/components/NavBar.svelte
@@ -0,0 +1,10 @@
+<div class=" fixed top-0 border-0 bg-[lightgoldenrodyellow] w-full h-16 z-10 flex"> <!-- navbar start -->
+    <div on:click={() => (location.href = "/")} class=" flex justify-start w-1/2"> <!-- left side -->
+        <button class=" px-9 text-2xl text-[#ff3e00] font-thin border-none ml-11" >IOTAHEROES</button>
+    </div>
+    <div class=" flex justify-end w-1/2"> <!-- right side -->
+        <button on:click={() => (location.href = "/trade")} class="mr-3 h-16 w-24 border-none text-lg hover:underline">TRADE</button>
+        <button on:click={() => (location.href = "/shop/0")} class=" mr-[12%] h-16 w-24 border-none text-lg hover:underline">SHOP</button>
+    </div>
+</div>
+<div class=" mb-16"></div>
