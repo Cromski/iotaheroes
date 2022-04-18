f9e92867fc8645b8264b2a4486552d8c2a40b9f5
Jakob Rasmus Henriksen <jarh@itu.dk>
2022-04-19 00:26:38 +0200

    Added a new media prefix for a responsive navbar

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ['./public/index.html', './src/**/*.svelte'],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'bing': '1000px',
+      },
+    },
   },
   plugins: [],
 }
